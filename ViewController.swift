4f951b44e4978cc857b41e02f96fd883d6c19271
DimaShelpuk <busikfaleot@gmail.com>
2026-04-19 12:45:42 +0300

    start

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by MacOS on 19.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
